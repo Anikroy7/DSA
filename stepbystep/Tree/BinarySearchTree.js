e4ddd2e7daf2d6f8be65f6635746070a083938ea
Anik Kumer Roy <96816764+Anikroy7@users.noreply.github.com>
2023-03-28 19:06:23 +0600

    binary search tree impelement

---
+++ stepbystep/Tree/BinarySearchTree.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isRootEmpty() {
+    return this.root === null;
+  }
+
+  makeTree(value) {
+    const newNode = new Node(value);
+    if (this.isRootEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertTree(this.root, newNode);
+    }
+  }
+  insertTree(root, node) {
+    if (node.value === root.value) {
+      console.warn("Value should be different! :)");
+      return;
+    }
+    if (node.value > root.value) {
+      if (root.right === null) {
+        root.right = node;
+      } else {
+        this.insertTree(root.right, node);
+      }
+    } else {
+      if (root.left === null) {
+        root.left = node;
+      }else{
+        this.insertTree(root.left, node)
+      }
+    }
+  }
+}
+
+const binarySearchTree = new BinarySearchTree();
+binarySearchTree.makeTree(30);
+binarySearchTree.makeTree(20);
+binarySearchTree.makeTree(25);
+binarySearchTree.makeTree(100);
+// binarySearchTree.makeTree(30);
+binarySearchTree.makeTree(40);
+binarySearchTree.makeTree(50);
+binarySearchTree.makeTree(60);
+binarySearchTree.makeTree(70);
+
+console.log(binarySearchTree);
